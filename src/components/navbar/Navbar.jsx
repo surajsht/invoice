82ae5dadcaa319a7567ea462cc021f1c738a2d42
suraj <surajsht2055@gmail.com>
2024-12-08 11:26:07 +0545

    theme status added in local storage

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,36 @@
+import { useState, useEffect } from "react";
 import { MdOutlineWbSunny } from "react-icons/md";
 import { FaRegMoon } from "react-icons/fa";
 import "./navbar.css";
 
 const Navbar = () => {
+  let [nightMode, setNightMode] = useState(false);
+
+  let toggleTheme = () => {
+    const isNight = !nightMode;
+    document.body.classList.toggle("night-mode");
+    localStorage.setItem("theme", isNight ? "night-mode" : "");
+    setNightMode(isNight);
+  };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "night-mode") {
+      document.body.classList.add("night-mode");
+      setNightMode(true);
+    }
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="wrapper">
         <div className="nav-content">
           <h1 className="logo"> Invoice </h1>
 
-          <button className="theme-toggle">
+          <button
+            className={`theme-toggle ${nightMode ? "night-mode" : ""}`}
+            onClick={toggleTheme}
+          >
             <MdOutlineWbSunny />
             <FaRegMoon />
           </button>
